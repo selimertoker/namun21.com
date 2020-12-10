8d853c432e7740a33ba9099326f4c8e8951a9a43
Derin Durak <durakderin@gmail.com>
2020-12-10 20:40:21 +0300

    Links js added

---
+++ js/links.js
@@ -0,0 +1,23 @@
+function uneplink(){
+    window.open("https://zoom.us/j/8584914526");
+}
+
+function uniceflink(){
+    window.open("https://zoom.us/j/2536304247");
+}
+
+function uncstdlink(){
+    window.open("https://zoom.us/j/2185457590");
+}
+
+function ioclink(){
+    window.open("https://zoom.us/j/3062296025");
+}
+
+function natolink(){
+    window.open("https://zoom.us/j/2074256064");
+}
+
+function rusrevlink(){
+    window.open("https://zoom.us/j/3565469193");
+}
