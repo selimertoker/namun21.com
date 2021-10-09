1804967ff355cb58b4e824da1e99abe18127224d
Derin Durak <durakderin@gmail.com>
2021-10-09 10:44:45 +0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -7,7 +7,7 @@ let countDown = new Date('Dec 24, 2021 8:00:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
-          distance = now - countDown;
+          distance = countDown - now;
 
       document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
